a847f3ec86f8bb9ee87a79b997f3ade6a18a2fcb
jeff.j.stringer@gmail.com <jeff.j.stringer@gmail.com>
2014-07-20 20:28:28 -0700

    updated css with background image

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
       sign_in subscriber
       redirect_to subscriber
     else
-      #flash.now[:error] = 'Invalid email/password combination'
+      flash.now[:error] = 'Invalid email/password combination'
       render 'new'
     end
   end
