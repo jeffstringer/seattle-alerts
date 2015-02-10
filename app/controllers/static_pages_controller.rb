f52d1178cb1e2f3ff5eae69049cdd3d35fc3a646
jeffstringer <jeff.j.stringer@gmail.com>
2015-02-10 10:49:08 -0800

    update main view of markers

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,7 @@
 class StaticPagesController < ApplicationController
   def index
-    unless current_subscriber == nil
-      @police_alerts = PoliceAlert.subscriber_alerts(current_subscriber)
-    else
-      @police_alerts = PoliceAlert.alerts
-    end
+    @current_subscriber = current_subscriber
+    current_subscriber.nil? ? @police_alerts = PoliceAlert.alerts : @police_alerts = PoliceAlert.subscriber_alerts(current_subscriber)
     @police_hash = Gmaps4rails.build_markers(@police_alerts) do |police_alert, marker|
       marker.lat(police_alert.latitude)
       marker.lng(police_alert.longitude)
@@ -18,11 +15,7 @@ class StaticPagesController < ApplicationController
       marker.infowindow render_to_string(:partial => '/layouts/police_alerts_infowindow', :locals => { :police_alert => police_alert } )
     end
 
-    unless current_subscriber == nil
-      @fire_alerts = FireAlert.subscriber_alerts(current_subscriber)
-    else
-      @fire_alerts = FireAlert.alerts
-    end
+    current_subscriber.nil? ? @fire_alerts = FireAlert.alerts : @fire_alerts = FireAlert.subscriber_alerts(current_subscriber)
     @fire_hash = Gmaps4rails.build_markers(@fire_alerts) do |fire_alert, marker|
       marker.lat(fire_alert.latitude)
       marker.lng(fire_alert.longitude)
@@ -35,19 +28,6 @@ class StaticPagesController < ApplicationController
       marker.infowindow render_to_string(:partial => '/layouts/fire_alerts_infowindow', :locals => { :fire_alert => fire_alert } )
     end
 
-    @current_subscriber = current_subscriber
-    @home = Gmaps4rails.build_markers(@current_subscriber) do |current_subscriber, marker|
-      marker.lat(current_subscriber.latitude)
-      marker.lng(current_subscriber.longitude)
-      marker.json({:id => current_subscriber.id })
-      marker.picture({
-        # http://mapicons.nicolasmollet.com/
-        "url" => view_context.image_path('/assets/home.png'),
-        "width" => 32,
-        "height" => 37
-      })
-      marker.infowindow render_to_string(:partial => '/layouts/current_subscriber_infowindow', :locals => { :subscriber => current_subscriber } )
-    end
   end
 
   private
