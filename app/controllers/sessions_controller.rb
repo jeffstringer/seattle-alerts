14afd7d297cfd99eb5418b340f18b9bccfd4ce7e
jeffstringer <jeff.j.stringer@gmail.com>
2015-02-10 13:34:33 -0800

    add link to update account in user signup email

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
       sign_in subscriber
       redirect_to root_url
     else
-      flash.now[:error] = 'Invalid email/password combination'
+      flash.now[:error] = 'Invalid email/password combination.'
       render 'new'
     end
   end
