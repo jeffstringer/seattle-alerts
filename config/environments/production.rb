26117ee0f8f7368a9ec8c54ab2d0cf3f59b53544
jeffstringer <jeff.j.stringer@gmail.com>
2015-02-11 16:06:08 -0800

    update config production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,6 +81,7 @@ SeattleAlerts::Application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'seattle-alerts.heroku.com' }
   config.action_mailer.smtp_settings = {
     :port           => ENV['MAILGUN_SMTP_PORT'],
     :address        => ENV['MAILGUN_SMTP_SERVER'],
