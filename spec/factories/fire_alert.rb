94145f7e063e9eb528fb5df44348fc904a69eca7
jeffstringer <jeff.j.stringer@gmail.com>
2015-06-13 16:16:22 -0700

    separate out factories

---
+++ spec/factories/fire_alert.rb
@@ -0,0 +1,13 @@
+FactoryGirl.define do
+  factory :fire_alert do
+    address "2132 Boyer Av E"
+    datetime "Thu Feb  5, 2015 at 10:26 AM"
+    incident_number "F150013312"
+    latitude 47.6101799
+    longitude -122.3423913 
+    fire_type "Auto Fire Alarm"
+    time_show "2015-02-05 18:26:00"
+    created_at "2015-02-06 18:02:56"
+    updated_at "2015-02-06 18:02:56"
+  end
+end
---
+++ spec/factories/subscriber.rb
@@ -0,0 +1,17 @@
+FactoryGirl.define do
+  factory :subscriber do
+    email 'chairman@starbucks.com'
+    street '1912 Pike Pl'
+    password 'coffee'
+    password_confirmation 'coffee'
+    radius 0.5
+  end
+
+  factory :subscriber2, class: Subscriber do
+    email 'taster@starbucks.com'
+    street '1912 Pike Pl'
+    password 'coffee'
+    password_confirmation 'coffee'
+    radius 0.5
+  end
+end
