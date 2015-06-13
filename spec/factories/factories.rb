01c7d7553a57dc331d99a47d9206c54103b53087
jeffstringer <jeff.j.stringer@gmail.com>
2015-06-13 15:27:37 -0700

    remove unnecessary factories and code

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -4,19 +4,7 @@ FactoryGirl.define do
     street '1912 Pike Pl'
     password 'coffee'
     password_confirmation 'coffee'
-    latitude 47.6101798
-    longitude -122.3423919
     radius 0.5
-  end
-
-  factory :subscriber_update, class: Subscriber do
-    email 'chairman@starbucks.com'
-    street '1912 Pike Pl'
-    password 'coffee'
-    password_confirmation 'coffee'
-    latitude 47.6101798
-    longitude -122.3423919
-    radius 1.0
   end
 
   factory :subscriber2, class: Subscriber do
@@ -24,8 +12,6 @@ FactoryGirl.define do
     street '1912 Pike Pl'
     password 'coffee'
     password_confirmation 'coffee'
-    latitude 47.6101798
-    longitude -122.3423919
     radius 0.5
   end
 
