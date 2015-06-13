cdb12c31b513998d64b77131b27c14b42580bdc3
jeffstringer <jeff.j.stringer@gmail.com>
2015-06-13 16:17:31 -0700

    update specs by including DataSupport module

--- spec/models/fire_alert_spec.rb
+++ spec/models/fire_alert_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe FireAlert do
+  include DataSupport
 
   it { should validate_presence_of :address }
   it { should validate_presence_of :datetime }
@@ -14,26 +15,10 @@ describe FireAlert do
 
   it { should have_many(:subscribers).through(:fire_notifications) }
 
-  array = [
-            {"address"=>"1000 4th Av",
-            "longitude"=>"-122.332909",
-            "latitude"=>"47.606088",
-            "incident_number"=>"F150019090",
-            "datetime"=>1424468700,
-            "type"=>"Auto Fire Alarm",
-            "report_location"=>{"needs_recoding"=>false, "longitude"=>"-122.332909", "latitude"=>"47.606088"}},
-            {"address"=>"3715 West Stevens Way Ne",
-            "longitude"=>"-122.307565",
-            "latitude"=>"47.652021",
-            "incident_number"=>"F150019128",
-            "datetime"=>1424475660,
-            "type"=>"Auto Fire Alarm",
-            "report_location"=>{"needs_recoding"=>false, "longitude"=>"-122.307565", "latitude"=>"47.652021"}}]
-
-  describe '.parse_fire_data(array)' do
+  describe '.parse_fire_data(raw_fire_data)' do
     it 'saves the data in psql' do
       FireAlert.destroy_all
-      FireAlert.parse_data(array)
+      FireAlert.parse_data(raw_fire_data)
       expect(FireAlert.first.address).to eq("1000 4th Av")
     end
   end             
