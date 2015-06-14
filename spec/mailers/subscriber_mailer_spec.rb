56f1c74ed40c95129730e215f1ba07f85797564e
jeffstringer <jeff.j.stringer@gmail.com>
2015-06-13 23:01:46 -0700

    fix specs post database_cleaner setup

--- spec/mailers/subscriber_mailer_spec.rb
+++ spec/mailers/subscriber_mailer_spec.rb
@@ -5,8 +5,8 @@ describe SubscriberMailer, :type => :mailer do
   let(:subscriber) { FactoryGirl.create(:subscriber) }
   let(:admin_mail) { SubscriberMailer.admin_email(subscriber) }
   let(:user_mail) { SubscriberMailer.signup_email(subscriber) }
-  let(:police_alert) { PoliceAlert.find_by(general_offense_number: '2014396010') }
-  let(:fire_alert) { FireAlert.find_by(incident_number: "F150013312") }
+  let(:police_alert) { FactoryGirl.create(:police_alert) }
+  let(:fire_alert) { FactoryGirl.create(:fire_alert) }
   let(:police_notification) { PoliceNotification.create(police_alert_id: police_alert.id, subscriber_id: subscriber.id) }
   let(:fire_notification) { FireNotification.create(fire_alert_id: fire_alert.id, subscriber_id: subscriber.id) }
   let(:notification_mail) { SubscriberMailer.notification_email([police_notification], [fire_notification], subscriber) }
--- spec/services/seattle_alert_spec.rb
+++ spec/services/seattle_alert_spec.rb
@@ -2,9 +2,9 @@ require 'spec_helper'
 
 describe SeattleAlert do
 
-  let(:subscriber) { Subscriber.find_by(email: "chairman@starbucks.com") }
-  let(:police_alert) { PoliceAlert.find_by(general_offense_number: '2014396010') }
-  let(:fire_alert) { FireAlert.find_by(incident_number: "F150013312") }
+  let(:subscriber) { FactoryGirl.create(:subscriber) }
+  let(:police_alert) { FactoryGirl.create(:police_alert) }
+  let(:fire_alert) { FactoryGirl.create(:fire_alert) }
   let(:police_notification) { PoliceNotification.create(police_alert_id: police_alert.id, subscriber_id: subscriber.id) }
   let(:fire_notification) { FireNotification.create(fire_alert_id: fire_alert.id, subscriber_id: subscriber.id) }
 
