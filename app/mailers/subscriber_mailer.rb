24fe3ad1e0264d7d11624acbe2ba56d12403c32a
jeffstringer <jeff.j.stringer@gmail.com>
2015-06-13 21:23:41 -0700

    refactor subscriber_mailer.rb

--- app/mailers/subscriber_mailer.rb
+++ app/mailers/subscriber_mailer.rb
@@ -33,27 +33,36 @@ class SubscriberMailer < ActionMailer::Base
     @police_alerts = []
     @fire_alerts = []
 
-    unless police_notifications.nil?
-      police_notifications.each do |p_n|
-        @police_alerts << p_n.police_alert if p_n.subscriber_id == subscriber.id
-      end
-      @police_alerts.sort_by! { |p| p[:time_show] }
-      @police_alerts.slice!(0,30) if @police_alerts.length >= 30
-      @police_alerts.reverse!
-    end
-
-    unless fire_notifications.nil?
-      fire_notifications.each do |f_n|
-        @fire_alerts << f_n.fire_alert if f_n.subscriber_id == subscriber.id
-      end
-      @fire_alerts.sort_by! { |f| f[:time_show] }
-      @fire_alerts.slice!(0,15) if @fire_alerts.length >= 30
-      @fire_alerts.reverse!
-    end
+    filter_police_alerts(police_notifications, fire_notifications, subscriber)
+    filter_fire_alerts(police_notifications, fire_notifications, subscriber)
 
     mail(to: subscriber.email, from: "Seattle Alerts <jeff.j.stringer@gmail.com>", subject: "Activity Occurred in Your Area") do |format|
       format.html 
       format.text
     end
   end
+
+  private
+
+    def filter_police_alerts(police_notifications, fire_notifications, subscriber)
+      unless police_notifications.nil?
+        police_notifications.each do |p_n|
+          @police_alerts << p_n.police_alert if p_n.subscriber_id == subscriber.id
+        end
+        @police_alerts.sort_by! { |p| p[:time_show] }
+        @police_alerts.slice!(0,30) if @police_alerts.length >= 30
+        @police_alerts.reverse!
+      end
+    end
+
+    def filter_fire_alerts(police_notifications, fire_notifications, subscriber)
+      unless fire_notifications.nil?
+        fire_notifications.each do |f_n|
+          @fire_alerts << f_n.fire_alert if f_n.subscriber_id == subscriber.id
+        end
+        @fire_alerts.sort_by! { |f| f[:time_show] }
+        @fire_alerts.slice!(0,15) if @fire_alerts.length >= 30
+        @fire_alerts.reverse!
+      end
+    end
 end
