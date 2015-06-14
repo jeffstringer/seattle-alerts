93318b57a001fa77c2d473a67bb54387abccf8b2
jeffstringer <jeff.j.stringer@gmail.com>
2015-06-13 18:57:45 -0700

    refactor spec/mailer/subscriber_mailer_spec.rb

--- spec/mailers/subscriber_mailer_spec.rb
+++ spec/mailers/subscriber_mailer_spec.rb
@@ -1,56 +1,49 @@
+require 'spec_helper'
 
-RSpec.describe SubscriberMailer, :type => :mailer do
-  describe "email sent to admin when new user signs up" do
-    let(:subscriber) { FactoryGirl.create(:subscriber) }
-    let(:mail) { SubscriberMailer.subscriber_email(subscriber) }
+describe SubscriberMailer, :type => :mailer do
 
+  let(:subscriber) { FactoryGirl.create(:subscriber) }
+  let(:admin_mail) { SubscriberMailer.admin_email(subscriber) }
+  let(:user_mail) { SubscriberMailer.signup_email(subscriber) }
+  let(:police_alert) { PoliceAlert.find_by(general_offense_number: '2014396010') }
+  let(:fire_alert) { FireAlert.find_by(incident_number: "F150013312") }
+  let(:police_notification) { PoliceNotification.create(police_alert_id: police_alert.id, subscriber_id: subscriber.id) }
+  let(:fire_notification) { FireNotification.create(fire_alert_id: fire_alert.id, subscriber_id: subscriber.id) }
+  let(:notification_mail) { SubscriberMailer.notification_email([police_notification], [fire_notification], subscriber) }
+
+  context "admin email upon signup" do
     it "renders the headers" do
-      expect(mail.subject).to eq("New Subscriber")
-      expect(mail.to).to eq(["jeff.j.stringer@gmail.com"])
-      expect(mail.from).to eq(["jeff.j.stringer@gmail.com"])
+      expect(admin_mail.subject).to eq("New Subscriber")
+      expect(admin_mail.to).to eq(["jeff.j.stringer@gmail.com"])
+      expect(admin_mail.from).to eq(["jeff.j.stringer@gmail.com"])
     end
 
     it "renders the body" do
-      expect(mail.body.encoded).to match(subscriber.email)
+      expect(admin_mail.body.encoded).to match(subscriber.email)
     end
   end
 
-  describe "email sent to user when user signs up" do
-    let(:subscriber) { FactoryGirl.create(:subscriber) }
-    let(:mail) { SubscriberMailer.signup_email(subscriber) }
-
+  context "user email upon signup" do
     it "renders the headers" do
-      expect(mail.subject).to eq("Welcome to Seattle Alerts")
-      expect(mail.to).to eq(["chairman@starbucks.com"])
-      expect(mail.from).to eq(["jeff.j.stringer@gmail.com"])
+      expect(user_mail.subject).to eq("Welcome to Seattle Alerts")
+      expect(user_mail.to).to eq(["chairman@starbucks.com"])
+      expect(user_mail.from).to eq(["jeff.j.stringer@gmail.com"])
     end
 
     it "renders the body" do
-      expect(mail.body.encoded).to match(subscriber.email)
+      expect(user_mail.body.encoded).to match(subscriber.email)
     end
   end
 
-  describe "email sent to user when activity occurrs" do
-    Subscriber.all.each {|s| s.destroy }
-    subscriber2 = FactoryGirl.create(:subscriber2)
-    PoliceAlert.all.each {|p| p.destroy }
-    police_alert = FactoryGirl.create(:police_alert)
-    FireAlert.all.each {|f| f.destroy }
-    fire_alert = FactoryGirl.create(:fire_alert)
-    PoliceNotification.all.each {|p| p.destroy }
-    police_notification = PoliceNotification.create(police_alert_id: police_alert.id, subscriber_id: subscriber2.id)
-    FireNotification.all.each {|f| f.destroy }
-    fire_notification = FireNotification.create(fire_alert_id: fire_alert.id, subscriber_id: subscriber2.id)
-    let(:mail) { SubscriberMailer.notification_email([police_notification], [fire_notification], subscriber2) }
-
+  context "user notification email when activity occurs" do
     it "renders the headers" do
-      expect(mail.subject).to eq("Activity Occurred in Your Area")
-      expect(mail.to).to eq(["taster@starbucks.com"])
-      expect(mail.from).to eq(["jeff.j.stringer@gmail.com"])
+      expect(notification_mail.subject).to eq("Activity Occurred in Your Area")
+      expect(notification_mail.to).to eq(["chairman@starbucks.com"])
+      expect(notification_mail.from).to eq(["jeff.j.stringer@gmail.com"])
     end
 
     it "renders the body" do
-      expect(mail.body.encoded).to match('Person With A Gun')
+      expect(notification_mail.body.encoded).to match('Person With A Gun')
     end
   end
 end
