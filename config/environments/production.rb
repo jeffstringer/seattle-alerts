2ff920af01dcf683f5ecf76e70e434fe0458eda5
jeffstringer <jeff.j.stringer@gmail.com>
2015-07-24 12:23:34 -0700

    modify config/environments/production.rb for mail settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,12 +78,13 @@ SeattleAlerts::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options = { :host => 'http://seattle-alerts.herokuapp.com/' }
-  config.action_mailer.smtp_settings = {
+  ActionMailer::Base..default_url_options = { :host => 'http://seattle-alerts.herokuapp.com/' }
+  ActionMailer::Base.smtp_settings = {
     :port           => ENV['MAILGUN_SMTP_PORT'],
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
     :authentication => :plain,
   }
+  ActionMailer::Base.delivery_method = :smtp
 end
