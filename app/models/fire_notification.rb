89683e6d9b8c9c99ef10cbb30fd67048f0ea4081
jeffstringer <jeff.j.stringer@gmail.com>
2015-09-19 21:45:39 -0700

    reorganize placement of notification module inclusion

--- app/models/fire_notification.rb
+++ app/models/fire_notification.rb
@@ -1,11 +1,11 @@
 class FireNotification < ActiveRecord::Base
 
+  class << self
+    include Notification
+  end
+
   belongs_to :subscriber
   belongs_to :fire_alert
 
   scope :new_notifications, -> { where("created_at >= ?", 15.minute.ago) }
-
-  class << self
-    include Notification
-  end
 end
--- app/models/police_notification.rb
+++ app/models/police_notification.rb
@@ -1,11 +1,11 @@
 class PoliceNotification < ActiveRecord::Base
 
+  class << self
+    include Notification
+  end
+
   belongs_to :subscriber
   belongs_to :police_alert
 
   scope :new_notifications, -> { where("created_at >= ?", 15.minute.ago) }
-
-  class << self
-    include Notification
-  end
 end
