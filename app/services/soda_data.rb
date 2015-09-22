524e3a041dc2cbfe6bc9ca2d8c475bf2c760c453
jeffstringer <jeff.j.stringer@gmail.com>
2015-09-22 09:22:32 -0700

    refactor SodaData#api_url

--- app/services/soda_data.rb
+++ app/services/soda_data.rb
@@ -1,11 +1,13 @@
 class SodaData
 
-  def initialize(api_url)
-    @api_url = api_url
+  attr_accessor :data_type
+
+  def initialize(data_type)
+    self.data_type = data_type
   end
 
   def fetch
-    url = "#{@api_url}?#{query}"
+    url = "#{api_url}?#{query}"
     url = URI.escape(url)
     json = open(url).read
     JSON.parse(json)
@@ -13,6 +15,14 @@ class SodaData
 
   def query
     timestamp = (Time.now - (60 * 60 * 24)).strftime('%F %H:%M:%S')
-    @api_url.include?('4ss6-4s75') ? query = "$where=datetime > '#{timestamp}'" : query = "$limit=1000"
-  end 
+    api_url.include?('4ss6-4s75') ? query = "$where=datetime > '#{timestamp}'" : query = "$limit=1000"
+  end
+
+  def api_url
+    if data_type.include?('fire')
+      return 'http://data.seattle.gov/resource/4ss6-4s75.json'
+    else
+      return 'http://data.seattle.gov/resource/fw4z-a47w.json'
+    end
+  end
 end
