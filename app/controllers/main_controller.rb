320bc4170f4a6ccc8b9ef6083da4d6852605a36f
jeffstringer <jeff.j.stringer@gmail.com>
2015-09-23 14:27:30 -0700

    refactor MainController and create methods for police, fire hashes

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,28 +1,33 @@
 class MainController < ApplicationController
   def index
-    @current_subscriber = current_subscriber
     current_subscriber.nil? ? @police_alerts = PoliceAlert.past_day_alerts : @police_alerts = PoliceAlert.subscriber_past_day_alerts(current_subscriber.id)
-    @police_hash = Gmaps4rails.build_markers(@police_alerts) do |police_alert, marker|
-      marker.lat(police_alert.latitude)
-      marker.lng(police_alert.longitude)
-      marker.json({:id => police_alert.id })
-      icon = IconSetter.call(police_alert.event_clearance_description)
-      marker.picture({ "url" => view_context.image_path(icon), "width" => 32, "height" => 37 })
-      marker.infowindow render_to_string(:partial => '/layouts/police_alerts_infowindow', :locals => { :police_alert => police_alert } )
-    end
+    @police_hash = police_hash
 
     current_subscriber.nil? ? @fire_alerts = FireAlert.past_day_alerts : @fire_alerts = FireAlert.subscriber_past_day_alerts(current_subscriber.id)
-    @fire_hash = Gmaps4rails.build_markers(@fire_alerts) do |fire_alert, marker|
-      marker.lat(fire_alert.latitude)
-      marker.lng(fire_alert.longitude)
-        # http://mapicons.nicolasmollet.com/
-      marker.picture({ "url" => view_context.image_path('fire.png'), "width" => 32, "height" => 37 })
-      marker.infowindow render_to_string(:partial => '/layouts/fire_alerts_infowindow', :locals => { :fire_alert => fire_alert } )
-    end
-
+    @fire_hash = fire_hash
   end
 
   private
+    def police_hash
+      Gmaps4rails.build_markers(@police_alerts) do |police_alert, marker|
+        marker.lat(police_alert.latitude)
+        marker.lng(police_alert.longitude)
+        marker.json({:id => police_alert.id })
+        icon = IconSetter.call(police_alert.event_clearance_description)
+        marker.picture({ "url" => view_context.image_path(icon), "width" => 32, "height" => 37 })
+        marker.infowindow render_to_string(:partial => '/layouts/police_alerts_infowindow', :locals => { :police_alert => police_alert } )
+      end
+    end
+
+    def fire_hash
+      Gmaps4rails.build_markers(@fire_alerts) do |fire_alert, marker|
+        marker.lat(fire_alert.latitude)
+        marker.lng(fire_alert.longitude)
+        marker.picture({ "url" => view_context.image_path('fire.png'), "width" => 32, "height" => 37 })
+        marker.infowindow render_to_string(:partial => '/layouts/fire_alerts_infowindow', :locals => { :fire_alert => fire_alert } )
+      end
+    end
+    
     def police_notification_params
       params.require(:police_notification).permit(:user_id, :police_alert_id)
     end
